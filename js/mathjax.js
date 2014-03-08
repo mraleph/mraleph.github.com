21c5d9182224d40223fed093669d95fc44d4d4a3
Vyacheslav Egorov <me@mrale.ph>
2014-03-08 11:24:12 +0100

    Create mathjax.js

---
+++ js/mathjax.js
@@ -0,0 +1,18 @@
+(function() {
+  var CONFIG = "MathJax.Hub.Config({"
+             + "  tex2jax: {"
+             + "    inlineMath: [['$','$'], ['\\(','\\)']],"
+             + "    skipTags: ['script','noscript','style','textarea']"
+             + "  }"
+             + "});";
+
+  var config = document.createElement("script");
+  config.type = "text/x-mathjax-config";
+  config.appendChild(document.createTextNode(CONFIG));
+  document.body.appendChild(config);
+
+  var script = document.createElement("script");
+  script.type = "text/javascript";
+  script.src = "https://c328740.ssl.cf1.rackcdn.com/mathjax/latest/MathJax.js?config=TeX-AMS-MML_HTMLorMML";
+  document.body.appendChild(script);
+})();
