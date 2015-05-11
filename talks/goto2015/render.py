44ae814b8a6256db641375f0403bef7242e144c7
Vyacheslav Egorov <me@mrale.ph>
2015-05-11 14:17:16 -0500

    Publish goto2015 slides
    Redeploy IRHydra

---
+++ talks/goto2015/render.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+
+#
+# Generate png slides from HTML slides.
+#
+
+import time
+from selenium import webdriver
+
+browser = webdriver.Chrome()
+browser.set_window_size(1024,768 + (768 - 696))
+browser.get('http://localhost:8000/talks/goto2015/')
+
+print browser.execute_script("return document.documentElement.clientWidth")
+print browser.execute_script("return document.documentElement.clientHeight")
+time.sleep(5)
+
+slide = 0
+while True:
+  time.sleep(0.5)
+  browser.save_screenshot('slides/slide-%d.png' % (slide))
+  if browser.execute_script("return Reveal.isLastSlide()"):
+    break
+  slide = slide + 1
+  browser.execute_script("Reveal.next()")
+  if slide % 10 == 0:
+    print slide
+
+browser.quit()
