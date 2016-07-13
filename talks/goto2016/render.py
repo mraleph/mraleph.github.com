4e2873f1937962e053b00394b3d282931799f2a3
Vyacheslav Egorov <me@mrale.ph>
2016-07-13 02:27:09 +0200

    Update goto2016 slides

--- talks/goto2016/render.py
+++ talks/goto2016/render.py
@@ -6,10 +6,18 @@
 
 import time
 from selenium import webdriver
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+
+
+#firefox_capabilities = DesiredCapabilities.FIREFOX
+#firefox_capabilities['marionette'] = True
+#firefox_capabilities['binary'] = '/Applications/FirefoxNightly.app/Contents/MacOS/firefox'
+
+#browser = webdriver.Firefox(capabilities=firefox_capabilities)
 
 browser = webdriver.Chrome()
-browser.set_window_size(1024,768 + (768 - 696))
-browser.get('http://localhost:8000/talks/goto2015/')
+browser.set_window_size(1280,720 + (720 - 646))
+browser.get('http://localhost:8000/talks/goto2016/')
 
 print browser.execute_script("return document.documentElement.clientWidth")
 print browser.execute_script("return document.documentElement.clientHeight")
@@ -18,7 +26,7 @@ time.sleep(5)
 slide = 0
 while True:
   time.sleep(0.5)
-  browser.save_screenshot('slides/slide-%d.png' % (slide))
+  browser.save_screenshot('slides/slide-%03d.png' % (slide))
   if browser.execute_script("return Reveal.isLastSlide()"):
     break
   slide = slide + 1
